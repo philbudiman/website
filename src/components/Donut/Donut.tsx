3df0da113dd089435e3778523d45f638ea51d52b
pbudiman <Phillipbudiman@gmail.com>
2025-02-17 18:39:09 -0500

    update donut

--- src/components/Donut/Donut.tsx
+++ src/components/Donut/Donut.tsx
@@ -1,14 +1,43 @@
 import React, { useRef, useEffect, useState } from 'react';
 
-const Donut: React.FC = () => {
+interface DonutProps {
+  width?: number;  // Width of the donut
+  interval?: number; // Interval to set animation speed
+  heightOverride?: number; // Height override of the donut (if no override is provided, height is 1/2 of width)
+}
+
+interface DonutData {
+  width: number;
+  height: number;
+  interval: number;
+}
+
+const donutDefaults = {
+  height: 35,
+  width: 80,
+  interval: 65
+}
+
+const getDimensions = (donutProps: DonutProps): DonutData => {
+  const width = donutProps.width ? donutProps.width : donutDefaults.width;
+  return {
+    width: width,
+    height: donutProps.heightOverride ? donutProps.heightOverride : width / 2,
+    interval: donutProps.interval ? donutProps.interval : donutDefaults.interval,
+  };
+};
+
+const Donut: React.FC<DonutProps> = (donutProps: DonutProps) => {
+  const { height, width, interval } = getDimensions(donutProps);
+  
   const preTagRef = useRef<HTMLPreElement | null>(null);
   const [tmr1, setTmr1] = useState<number | undefined>(undefined);
 
   let A = 1, B = 1;
 
   const asciiFrame = () => {
-    const b: string[] = Array(1760).fill(" ");
-    const z: number[] = Array(1760).fill(0);
+    const b: string[] = Array(width * height).fill(" ");
+    const z: number[] = Array(width * height).fill(0);
 
     A += 0.07;
     B += 0.03;
@@ -16,10 +45,12 @@ const Donut: React.FC = () => {
     const cA = Math.cos(A), sA = Math.sin(A),
       cB = Math.cos(B), sB = Math.sin(B);
 
-    for (let k = 79; k < 1760; k += 80) {
+    // Set up the newline at the end of each row
+    for (let k = width - 1; k < width * height; k += width) {
       b[k] = "\n";
-    }    
-    
+    }
+
+    // Loop for generating the 3D effect
     for (let j = 0; j < 6.28; j += 0.07) {  // theta
       const ct = Math.cos(j), st = Math.sin(j);
       for (let i = 0; i < 6.28; i += 0.02) {  // phi
@@ -27,12 +58,12 @@ const Donut: React.FC = () => {
         const h = ct + 2;  // R1 + R2*cos(theta)
         const D = 1 / (sp * h * sA + st * cA + 5);  // 1/z
         const t = sp * h * cA - st * sA;  // factor for x' and y'
-        const x = Math.floor(40 + 30 * D * (cp * h * cB - t * sB));
-        const y = Math.floor(12 + 15 * D * (cp * h * sB + t * cB));
-        const o = x + 80 * y;
+        const x = Math.floor(width / 2 + (width / 2) * D * (cp * h * cB - t * sB));
+        const y = Math.floor(height / 2 + (height / 2) * D * (cp * h * sB + t * cB));
+        const o = x + width * y;
         const N = Math.floor(8 * ((st * sA - sp * ct * cA) * cB - sp * ct * sA - st * cA - cp * ct * sB));
 
-        if (y < 22 && y >= 0 && x >= 0 && x < 79 && D > z[o]) {
+        if (y < height && y >= 0 && x >= 0 && x < width && D > z[o]) {
           z[o] = D;
           b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
         }
@@ -46,8 +77,8 @@ const Donut: React.FC = () => {
 
   const startAnimation = () => {
     if (tmr1 === undefined) {
-      const interval = setInterval(asciiFrame, 65);
-      setTmr1(interval);
+      const intervalId = setInterval(asciiFrame, interval);
+      setTmr1(intervalId);
     }
   };
 
@@ -61,7 +92,7 @@ const Donut: React.FC = () => {
   useEffect(() => {
     startAnimation();
     return stopAnimation;
-  });
+  }, [interval]); // Add interval as a dependency to restart the animation with a new interval if it changes.
 
   return (
     <div>
