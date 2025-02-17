9d68dd3aeec321ab47abc1166665af4077901091
pbudiman <Phillipbudiman@gmail.com>
2025-02-17 18:18:15 -0500

    clean up donut

--- src/components/Donut/Donut.tsx
+++ src/components/Donut/Donut.tsx
@@ -1,22 +1,24 @@
 import React, { useRef, useEffect, useState } from 'react';
 
 const Donut: React.FC = () => {
-  const pretagRef = useRef<HTMLPreElement | null>(null);
+  const preTagRef = useRef<HTMLPreElement | null>(null);
   const [tmr1, setTmr1] = useState<number | undefined>(undefined);
+
   let A = 1, B = 1;
 
-  const asciiframe = () => {
-    const b: string[] = [];
-    const z: number[] = [];
+  const asciiFrame = () => {
+    const b: string[] = Array(1760).fill(" ");
+    const z: number[] = Array(1760).fill(0);
+
     A += 0.07;
     B += 0.03;
+
     const cA = Math.cos(A), sA = Math.sin(A),
       cB = Math.cos(B), sB = Math.sin(B);
-    
-    for (let k = 0; k < 1760; k++) {
-      b[k] = k % 80 === 79 ? "\n" : " ";
-      z[k] = 0;
-    }
+
+    for (let k = 79; k < 1760; k += 80) {
+      b[k] = "\n";
+    }    
     
     for (let j = 0; j < 6.28; j += 0.07) {  // theta
       const ct = Math.cos(j), st = Math.sin(j);
@@ -29,6 +31,7 @@ const Donut: React.FC = () => {
         const y = Math.floor(12 + 15 * D * (cp * h * sB + t * cB));
         const o = x + 80 * y;
         const N = Math.floor(8 * ((st * sA - sp * ct * cA) * cB - sp * ct * sA - st * cA - cp * ct * sB));
+
         if (y < 22 && y >= 0 && x >= 0 && x < 79 && D > z[o]) {
           z[o] = D;
           b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
@@ -36,15 +39,15 @@ const Donut: React.FC = () => {
       }
     }
 
-    if (pretagRef.current) {
-      pretagRef.current.innerHTML = b.join('');
+    if (preTagRef.current) {
+      preTagRef.current.innerHTML = b.join('');
     }
   };
 
   const startAnimation = () => {
     if (tmr1 === undefined) {
-      const interval1 = setInterval(asciiframe, 65);
-      setTmr1(interval1);
+      const interval = setInterval(asciiFrame, 65);
+      setTmr1(interval);
     }
   };
 
@@ -56,18 +59,13 @@ const Donut: React.FC = () => {
   };
 
   useEffect(() => {
-    // Start animation when the component mounts
     startAnimation();
-
-    return () => {
-      // Cleanup when the component unmounts
-      stopAnimation();
-    };
-  }, [tmr1]);
+    return stopAnimation;
+  });
 
   return (
     <div>
-      <pre ref={pretagRef}></pre>
+      <pre ref={preTagRef}></pre>
     </div>
   );
 };
