7e15393ddee56734214de53727b8cee46da07675
pbudiman <Phillipbudiman@gmail.com>
2025-05-18 21:27:36 -0700

    update resume URL

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -5,7 +5,7 @@ import Markdown from 'react-markdown';
 const About = () => {
   const textContent = [
     "hey! i'm phil. i'm a **software engineer** at **eightfold.ai** currently based in **san jose, ca**",
-    "i graduated from boston university in 2023 with a b.a. in computer science and a minor in economics.\nyou can check out my resume [here](https://drive.google.com/file/d/1WLEl-PwuPNVvJpCtIbgV0prk3HmJSz3S/view?usp=sharing)",
+    "i graduated from boston university in 2023 with a b.a. in computer science and a minor in economics.\nyou can check out my resume [here](https://drive.google.com/file/d/1k3o7g6inPE2JwOwuCIm-oUiTBCLH3PUi/view?usp=sharing)",
     "i'm passionate about:\n(1) building great user experiences\n(2) working across the stack\n(3) tinkering with vintage technology",
   ];
   return (
